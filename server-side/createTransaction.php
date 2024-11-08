e0400771539701243922baf15590f0e0c349a2a1
Sana <Lenovo@DESKTOP-I4OODFD>
2024-11-08 23:48:03 +0200

    Edit - get all transactions function

--- server-side/createTransaction.php
+++ server-side/createTransaction.php
@@ -1,29 +1,32 @@
 <?php
 
 include "connection.php";
-// $type = $_POST["type"] ;
-// $amount = $_POST["amount"];
-// $date = $_POST["date"] ;
-// $notes = $_POST["notes"] ;
+$type = $_POST["type"] ;
+$amount = $_POST["amount"];
+$date = $_POST["date"] ;
+$notes = $_POST["notes"] ;
 
-$type = $_GET["type"] ;
-$amount = $_GET["amount"];
-$date = $_GET["date"] ;
-$notes = $_GET["notes"] ;
+// $type = $_GET["type"] ;
+// $amount = $_GET["amount"];
+// $date = $_GET["date"] ;
+// $notes = $_GET["notes"] ;
 
-$id = 1;
+$user_id = 1;
 
 $query = $connection->prepare("INSERT INTO transactions (type, amount, date, notes, Users_id) VALUES (?, ?, ?, ?, ?)");
 
-$query->bind_param("sdssi", $type, $amount, $date, $notes, $id);
+$query->bind_param("sdssi", $type, $amount, $date, $notes, $user_id);
 
 $query->execute();
 if($connection->affected_rows >0){
-    echo "successful";
+    echo  json_encode([
+        "message"=>"Successful connection"
+    ]);
 }
 else{
-    
-    echo $connection->error;
+    echo  json_encode([
+        "message"=>$connection->error
+    ]);
 }
 
 
